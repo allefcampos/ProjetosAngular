de2c17b86f5b6479104ddd86bad9fbc297e2769a
Allef Campos <allef-cleiton88@hotmail.com>
2022-08-05 00:23:32 -0300

    Implementação dos testes do angular

---
+++ 13 - Testes Unidade Integracao/src/app/demos/pipes/filmes/image.pipe.spec.ts
@@ -0,0 +1,46 @@
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { Component } from "@angular/core";
+import { ImageFormaterPipe } from "./image.pipe";
+
+fdescribe("ImageFormaterPipe", () => {
+  const imagePipe = new ImageFormaterPipe();
+
+  it("Deve transformar o caminho da imagem padrão", () => {
+    expect(imagePipe.transform("", "default", true)).toEqual(
+      "/assets/semCapa.jpg"
+    );
+  });
+
+  it("Deve validar o caminho padrão do teste", () => {
+    expect(imagePipe.transform("teste.png", "img", false)).not.toEqual(
+      "/assets/semCapa.jpg"
+    );
+  });
+
+  describe("testando comportamental do pipe", () => {
+    @Component({
+      template: `Imagem: {{ img | imageformater:teste }} `,
+    })
+
+    class ImagemComponent {
+      img = "teste.png";
+      caminho = "allef";
+    }
+
+    let component: ImagemComponent;
+    let fixture: ComponentFixture<ImagemComponent>;
+    let el: HTMLElement;
+
+    beforeEach(() => {
+      TestBed.configureTestingModule({
+        declarations: [ImageFormaterPipe, ImagemComponent],
+      });
+
+      fixture = TestBed.createComponent(ImagemComponent);
+      component = fixture.componentInstance;
+      el = fixture.nativeElement;
+    });
+
+    //TODO: Finalizar implementação do teste.
+  });
+});
---
+++ 13 - Testes Unidade Integracao/src/app/demos/pipes/filmes/image.pipe.ts
@@ -0,0 +1,21 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+    name: 'imageformater'
+})
+export class ImageFormaterPipe implements PipeTransform {
+    transform(imagem: string, caminho: string = '', substituir: boolean = true) {
+        console.log(caminho);
+        
+        if (caminho == 'default')
+            caminho = 'assets';
+        else 
+            caminho = 'padrao'
+
+        if (imagem.length == 0 && substituir) {
+            imagem = 'semCapa.jpg'
+        }
+
+        return "/" + caminho + "/" + imagem
+    }
+}
