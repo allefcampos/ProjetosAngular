94d6f2cbb83df92f2e5a13e3c6354ff553a395c2
Allef Campos <allef-cleiton88@hotmail.com>
2022-08-09 21:16:15 -0300

    refatoração

--- 14 - ProjetoFinal/front-end/06 - Projeto Concluido/src/app/base-components/form-base.component.ts
+++ 14 - ProjetoFinal/front-end/06 - Projeto Concluido/src/app/base-components/form-base.component.ts
@@ -3,34 +3,40 @@ import { FormGroup } from '@angular/forms';
 
 import { Observable, fromEvent, merge } from 'rxjs';
 
-import { GenericValidator, DisplayMessage, ValidationMessages } from '../utils/generic-form-validation';
+import {
+  GenericValidator,
+  DisplayMessage,
+  ValidationMessages,
+} from '../utils/generic-form-validation';
 
 export abstract class FormBaseComponent {
+  displayMessage: DisplayMessage = {};
+  genericValidator: GenericValidator;
+  validationMessages: ValidationMessages;
 
-    displayMessage: DisplayMessage = {};
-    genericValidator: GenericValidator;
-    validationMessages: ValidationMessages;
+  mudancasNaoSalvas: boolean;
 
-    mudancasNaoSalvas: boolean;
+  protected configurarMensagensValidacaoBase(
+    validationMessages: ValidationMessages
+  ) {
+    this.genericValidator = new GenericValidator(validationMessages);
+  }
 
-    protected configurarMensagensValidacaoBase(validationMessages: ValidationMessages) {
-        this.genericValidator = new GenericValidator(validationMessages);
-    }
+  protected configurarValidacaoFormularioBase(
+    formInputElements: ElementRef[],
+    formGroup: FormGroup
+  ) {
+    let controlBlurs: Observable<any>[] = formInputElements.map(
+      (formControl: ElementRef) => fromEvent(formControl.nativeElement, 'blur')
+    );
 
-    protected configurarValidacaoFormularioBase(
-        formInputElements: ElementRef[],
-        formGroup: FormGroup) {
+    merge(...controlBlurs).subscribe(() => {
+      this.validarFormulario(formGroup);
+    });
+  }
 
-        let controlBlurs: Observable<any>[] = formInputElements
-            .map((formControl: ElementRef) => fromEvent(formControl.nativeElement, 'blur'));
-
-        merge(...controlBlurs).subscribe(() => {
-            this.validarFormulario(formGroup)
-        });
-    }
-
-    protected validarFormulario(formGroup: FormGroup) {
-        this.displayMessage = this.genericValidator.processarMensagens(formGroup);
-        this.mudancasNaoSalvas = true;
-    }
+  protected validarFormulario(formGroup: FormGroup) {
+    this.displayMessage = this.genericValidator.processarMensagens(formGroup);
+    this.mudancasNaoSalvas = true;
+  }
 }
