1288e2344814267a0730e767504351848add11f8
Allef Campos <allef-cleiton88@hotmail.com>
2022-08-16 23:54:05 -0300

    criação do test do component

--- 16 - Angular - testes automatizados com jasmine e karma/src/app/shared/components/like-widget/like-widget.module.ts
+++ 16 - Angular - testes automatizados com jasmine e karma/src/app/shared/components/like-widget/like-widget.module.ts
@@ -1,15 +1,13 @@
 import { CommonModule } from '@angular/common';
-import { EventEmitter, Input, NgModule, Output } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { UniqueIdService } from '../../services/unique-id/unique-id.service';
 import { LikeWidgetComponent } from './like-widget.component';
 
 @NgModule({
   declarations: [LikeWidgetComponent],
   imports: [CommonModule, FontAwesomeModule],
   exports: [LikeWidgetComponent],
+  providers: [UniqueIdService]
 })
-export class LikeWidgetModule {
-  @Output() public liked = new EventEmitter<void>();
-  @Input() public likes = 0;
-  @Input() public id = null;
-}
+export class LikeWidgetModule {}
