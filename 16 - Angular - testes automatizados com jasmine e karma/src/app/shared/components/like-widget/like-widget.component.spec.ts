cb9ef89a8754c6e805fa38e57a24d167b251ca06
Allef Campos <allef-cleiton88@hotmail.com>
2022-08-17 23:37:45 -0300

    test component

--- 16 - Angular - testes automatizados com jasmine e karma/src/app/shared/components/like-widget/like-widget.component.spec.ts
+++ 16 - Angular - testes automatizados com jasmine e karma/src/app/shared/components/like-widget/like-widget.component.spec.ts
@@ -2,24 +2,56 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { UniqueIdService } from '../../services/unique-id/unique-id.service';
 import { LikeWidgetComponent } from './like-widget.component';
+import { LikeWidgetModule } from './like-widget.module';
 
 describe(LikeWidgetComponent.name, () => {
-
   // criando fixture do component
   let fixture: ComponentFixture<LikeWidgetComponent> = null;
+  let component: LikeWidgetComponent = null;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      // Pode importar o módulo para não precisar importar toda a estrutura
+      // imports: [LikeWidgetModule]
       declarations: [LikeWidgetComponent],
       providers: [UniqueIdService],
-      imports: [FontAwesomeModule]
+      imports: [FontAwesomeModule],
     }).compileComponents();
 
     fixture = TestBed.createComponent(LikeWidgetComponent);
+    component = fixture.componentInstance;
   });
 
   it('Should create component', () => {
-    const instance = fixture.componentInstance;
-    expect(instance).toBeTruthy();
+    expect(component).toBeTruthy();
   });
+
+  it('Should auto-generate ID during ngOnInput when (@Input id) is not assigned', () => {
+    fixture.detectChanges();
+    expect(component.id).toBeTruthy();
+  });
+
+  it('Should NOT auto-generate ID during ngOnInit when (@Input id) is assigned', () => {
+    const someId = 'someId';
+    component.id = someId;
+    fixture.detectChanges();
+
+    expect(component.id).toBe(someId);
+  });
+
+  it(`#${LikeWidgetComponent.prototype.like.name} should trigger (@Output liked) when called`, done => {
+    fixture.detectChanges();
+    component.liked.subscribe(() => {
+      expect(true).toBeTrue();
+      done();
+    });
+    component.like();
+  })
+
+  it(`#${LikeWidgetComponent.prototype.like.name} should trigger emission when called 2`, () => {
+    spyOn(component.liked, 'emit');
+    fixture.detectChanges();
+    component.like();
+    expect(component.liked.emit).toHaveBeenCalled();
+  })
 });
