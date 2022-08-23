07b17656828009d9d0310e135b16a05a88ad5791
Allef Campos <allef-cleiton88@hotmail.com>
2022-08-22 22:24:36 -0300

    implementação novos testes

---
+++ 17 - Angular - testes automatizados com jasmine e karma + server/src/app/shared/components/photo-frame/photo-frame.component.ts
@@ -0,0 +1,43 @@
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnDestroy,
+  OnInit,
+  Output,
+} from '@angular/core';
+import { Subject } from 'rxjs';
+import { debounceTime, takeUntil } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-photo-frame',
+  templateUrl: 'photo-frame.component.html',
+  styleUrls: ['photo-frame.component.scss'],
+})
+export class PhotoFrameComponent implements OnInit, OnDestroy {
+  @Input() description = '';
+  @Input() src = '';
+  @Input() likes = 0;
+
+  @Output() public liked: EventEmitter<void> = new EventEmitter();
+
+  private debounceSubject: Subject<void> = new Subject();
+  private unsubscribe: Subject<void> = new Subject();
+
+  ngOnInit(): void {
+    this.debounceSubject
+      .asObservable()
+      .pipe(debounceTime(500))
+      .pipe(takeUntil(this.unsubscribe))
+      .subscribe(() => this.liked.emit());
+  }
+
+  ngOnDestroy(): void {
+    this.unsubscribe.next();
+    this.unsubscribe.complete();
+  }
+
+  public like() {
+    this.debounceSubject.next();
+  }
+}
