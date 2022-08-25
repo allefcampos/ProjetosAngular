13ed0dd1f69568bea213e48a6570952ade5d5a30
Allef Campos <allef-cleiton88@hotmail.com>
2022-08-24 23:40:53 -0300

    novos testes

--- 17 - Angular - testes automatizados com jasmine e karma + server/src/app/shared/components/like-widget/like-widget.component.spec.ts
+++ 17 - Angular - testes automatizados com jasmine e karma + server/src/app/shared/components/like-widget/like-widget.component.spec.ts
@@ -39,18 +39,18 @@ describe(LikeWidgetComponent.name, () => {
       expect(component.liked.emit).toHaveBeenCalled();
   });
 
-  it(`(D) Should display number of likes when clicked`, done => {
-      fixture.detectChanges();
-      component.liked.subscribe(() => {
-        component.likes++;
-        fixture.detectChanges();
-        const count: HTMLElement = fixture.nativeElement.querySelector('.like-counter');
-        expect(count.textContent.trim()).toBe('1');
-        done();
-      });
+  // it(`(D) Should display number of likes when clicked`, done => {
+  //     fixture.detectChanges();
+  //     component.liked.subscribe(() => {
+  //       component.likes++;
+  //       fixture.detectChanges();
+  //       const count: HTMLElement = fixture.nativeElement.querySelector('.like-counter');
+  //       expect(count.textContent.trim()).toBe('1');
+  //       done();
+  //     });
 
-      const likeWidgetContainer: HTMLElement = fixture.nativeElement
-        .querySelector('.like-widget-container');
-        likeWidgetContainer.click();
-  });
+  //     const likeWidgetContainer: HTMLElement = fixture.nativeElement
+  //       .querySelector('.like-widget-container');
+  //       likeWidgetContainer.click();
+  // });
 });
